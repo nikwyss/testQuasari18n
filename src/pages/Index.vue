c2b33702eba2334c0e169a3676aba2c71973c5aa
Dominik Wyss <nikwyss@gmail.com>
2022-01-24 13:41:17 +0100

    Initial commit

---
+++ src/pages/Index.vue
@@ -0,0 +1,31 @@
+<template>
+  <q-page class="flex flex-center">
+    <p>
+      Testing I18n Translation on Production Environments with restrictive CSP
+      Header.
+    </p>
+
+    <br /><br />
+
+    <p>
+      <i>{{ $t("example") }}</i>
+    </p>
+
+    <!-- ADD CSP Header-->
+    <teleport to="head" v-if="prod">
+      <meta http-equiv="Content-Security-Policy" content="default-src 'self'" />
+    </teleport>
+  </q-page>
+</template>
+
+<script>
+import { defineComponent } from "vue";
+
+export default defineComponent({
+  name: "PageIndex",
+  setup() {
+    const prod = true; // process.env.NODE_ENV != "production";
+    return { prod };
+  },
+});
+</script>
